ba187c5568645b51ac450abd5dd753a4442af308
GayathriVadapalli <gayathrisphg21@gmail.com>
2025-08-20 23:42:04 +0530

    Fix all configuration errors for Netlify deployment: tsconfig.json, ESLint, and dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,18 +3,24 @@ module.exports = {
   env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-unused-vars': 'off', // Turn off for TypeScript files
     'react-hooks/exhaustive-deps': 'warn',
   },
 }
